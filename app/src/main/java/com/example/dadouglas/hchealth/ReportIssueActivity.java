7d230fada631ce2c82299713d814c42ffc4eeaac
dariusdouglas <darius.a.douglas@gmail.com>
2017-01-30 13:08:34 -0600

    Upgrades to Report Issue Form

--- app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
+++ app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
@@ -1,7 +1,12 @@
 package com.example.dadouglas.hchealth;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ReportIssueActivity extends AppCompatActivity {
 
@@ -10,4 +15,13 @@ public class ReportIssueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_issue);
     }
+
+    // Check if no view has focus:
+//    View view = this.getCurrentFocus();
+//    if (view != null) {
+//        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+//    }
+
+
 }
