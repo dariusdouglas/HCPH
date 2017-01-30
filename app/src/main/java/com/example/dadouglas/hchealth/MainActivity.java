978f2cfe5a69584a88dc3fabee14fe71ea65c362
dariusdouglas <darius.a.douglas@gmail.com>
2017-01-30 10:06:43 -0600

    Push Remaining Code into Repository

---
+++ app/src/main/java/com/example/dadouglas/hchealth/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dadouglas.hchealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Start report
+    public void startReportIssueActivity(View view) {
+        Intent intent = new Intent(this, ReportIssueActivity.class);
+        startActivity(intent);
+    }
+}
