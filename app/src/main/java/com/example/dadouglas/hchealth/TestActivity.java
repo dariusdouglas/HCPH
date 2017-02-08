eb9806c10b3fa5a9af57a49fafac9dab5e34b2cf
dariusdouglas <darius.a.douglas@gmail.com>
2017-02-08 15:54:15 -0600

    Updated activity_main, gridview_test, and activity_report to better accommodate bigger screen sizes

---
+++ app/src/main/java/com/example/dadouglas/hchealth/TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.dadouglas.hchealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridview_test);
+    }
+
+}
