084855bc5dd0d2bbcdc86a2791825db7b664bd20
dariusdouglas <darius.a.douglas@gmail.com>
2017-02-28 15:21:55 -0600

    Use JSONArray to access values returned from ReportIssueSubject service

--- app/src/main/java/com/example/dadouglas/hchealth/GetIssueSUbjects.java
+++ app/src/main/java/com/example/dadouglas/hchealth/GetIssueSUbjects.java
@@ -1,7 +1,10 @@
 package com.example.dadouglas.hchealth;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,6 +16,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+
 
 /**
  * Created by DaDouglas on 2/27/2017.
@@ -24,6 +29,7 @@ public class GetIssueSubjects extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground(String... params) {
         String link = "http://appsqa.harriscountytx.gov/PublicHealthMobile/publichealth.svc/ReportIssueSubject";
+
         try {
             URL url = new URL(link);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -36,11 +42,23 @@ public class GetIssueSubjects extends AsyncTask<String, Void, Void> {
             while ((data = reader.readLine()) != null) {
                 webPage += data + "\n";
             }
-
             try {
                 JSONObject jObject = new JSONObject(webPage);
                 JSONArray jsonArray = jObject.getJSONArray("GetReportIssueSubjectResult");
                 Log.d(ReportIssueActivity.class.getSimpleName(), jsonArray.get(0) + "taggggggggggggggg");
+
+                Intent intent = new Intent(ReportIssueActivity.class.getSimpleName());
+                intent.putExtra("test", "val");
+                HashMap<String, String> applicationSettings = new HashMap<String, String>();
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String value = jsonArray.getJSONObject(i).getString("Name");
+                    String name = jsonArray.getJSONObject(i).getString("ID");
+
+//                    intent.putExtra("test", "val");
+//                    applicationSettings.put(name, value);
+//                    Log.d(ReportIssueActivity.class.getSimpleName(), "JSONOBJ1: " + value);
+//                    Log.d(ReportIssueActivity.class.getSimpleName(), name);
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -52,5 +70,9 @@ public class GetIssueSubjects extends AsyncTask<String, Void, Void> {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String test() {
+        return "testing Return value";
     }
 }
--- app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
+++ app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
@@ -1,6 +1,7 @@
 package com.example.dadouglas.hchealth;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,14 +29,27 @@ import java.net.URLConnection;
 
 
 public class ReportIssueActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_issue);
-
         GetIssueSubjects getIssuesRequest = new GetIssueSubjects();
         getIssuesRequest.execute();
+
+        Intent intent = getIntent();
+
+        Log.d(ReportIssueActivity.class.getSimpleName(), intent.getStringExtra("test")+"tagrrrrrrr");
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, android.R.id.text1);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinnerAdapter.add("value");
+        spinnerAdapter.notifyDataSetChanged();
+
+
 
     }
 }
