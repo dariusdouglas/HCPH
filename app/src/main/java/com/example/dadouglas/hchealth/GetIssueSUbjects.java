fe83424ea0f262af259464f6e44f7ff071bcd7f7
dariusdouglas <darius.a.douglas@gmail.com>
2017-03-01 07:19:29 -0600

    Removed KSOAP library
    Updated ReportIssueActivity

--- app/src/main/java/com/example/dadouglas/hchealth/GetIssueSUbjects.java
+++ app/src/main/java/com/example/dadouglas/hchealth/GetIssueSUbjects.java
@@ -1,10 +1,7 @@
 package com.example.dadouglas.hchealth;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,18 +13,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 
 
-/**
- * Created by DaDouglas on 2/27/2017.
- */
-
-
-public class GetIssueSubjects extends AsyncTask<String, Void, Void> {
+public class GetIssueSubjects extends AsyncTask<String, Void, JSONArray> {
 
     @Override
-    protected Void doInBackground(String... params) {
+    protected JSONArray doInBackground(String... params) {
         String link = "http://appsqa.harriscountytx.gov/PublicHealthMobile/publichealth.svc/ReportIssueSubject";
 
         try {
@@ -42,37 +33,18 @@ public class GetIssueSubjects extends AsyncTask<String, Void, Void> {
             while ((data = reader.readLine()) != null) {
                 webPage += data + "\n";
             }
-            try {
-                JSONObject jObject = new JSONObject(webPage);
-                JSONArray jsonArray = jObject.getJSONArray("GetReportIssueSubjectResult");
-                Log.d(ReportIssueActivity.class.getSimpleName(), jsonArray.get(0) + "taggggggggggggggg");
+            JSONObject jObject = new JSONObject(webPage);
+            JSONArray jsonArray = jObject.getJSONArray("GetReportIssueSubjectResult");
 
-                Intent intent = new Intent(ReportIssueActivity.class.getSimpleName());
-                intent.putExtra("test", "val");
-                HashMap<String, String> applicationSettings = new HashMap<String, String>();
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    String value = jsonArray.getJSONObject(i).getString("Name");
-                    String name = jsonArray.getJSONObject(i).getString("ID");
-
-//                    intent.putExtra("test", "val");
-//                    applicationSettings.put(name, value);
-//                    Log.d(ReportIssueActivity.class.getSimpleName(), "JSONOBJ1: " + value);
-//                    Log.d(ReportIssueActivity.class.getSimpleName(), name);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            Log.d(ReportIssueActivity.class.getSimpleName(), webPage);
+            return jsonArray;
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (java.io.IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         return null;
-    }
-
-    public String test() {
-        return "testing Return value";
     }
 }
--- app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
+++ app/src/main/java/com/example/dadouglas/hchealth/ReportIssueActivity.java
@@ -1,55 +1,53 @@
 package com.example.dadouglas.hchealth;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.JSONException;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 
 public class ReportIssueActivity extends AppCompatActivity {
     Spinner spinner;
+    JSONArray subjectsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_issue);
+
+        // Call service to get Issue subjects and populate dropdown menu with subjects
         GetIssueSubjects getIssuesRequest = new GetIssueSubjects();
-        getIssuesRequest.execute();
 
-        Intent intent = getIntent();
+        try {
+            // Call GetIssueSubject service
+            subjectsArray = getIssuesRequest.execute().get();
 
-        Log.d(ReportIssueActivity.class.getSimpleName(), intent.getStringExtra("test")+"tagrrrrrrr");
-        spinner = (Spinner)findViewById(R.id.spinner);
-
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, android.R.id.text1);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
-        spinnerAdapter.add("value");
-        spinnerAdapter.notifyDataSetChanged();
+            // Get spinner item
+            spinner = (Spinner) findViewById(R.id.spinner);
+            ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, android.R.id.text1);
+            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(spinnerAdapter);
 
 
+            // add values to spinner
+            for (int i = 0; i < subjectsArray.length(); i++) {
+                String value = subjectsArray.getJSONObject(i).getString("Name");
+                spinnerAdapter.add(value);
 
+            }
+
+            //update spinner
+            spinnerAdapter.notifyDataSetChanged();
+
+        }
+        catch (InterruptedException e) { e.printStackTrace(); }
+        catch (ExecutionException e) { e.printStackTrace(); }
+        catch (JSONException e) { e.printStackTrace(); }
     }
 }
